719992d1e0fbac06701b267c4f5b36491af27dbf
Arnau Allo <arnau.allo@enti.cat>
2019-11-24 14:43:29 +0100

    Backup

--- SDL_Pathfinding/src/Grid.cpp
+++ SDL_Pathfinding/src/Grid.cpp
@@ -286,121 +286,209 @@ Path Grid::getPathBFS(Vector2D start, Vector2D end)
 Path Grid::getPathDjikstra(Vector2D start, Vector2D end) {
 	std::deque<MyNode> visitedCells;
 	MyNode n = MyNode(start, nullptr, 0, 0);
-	visitedCells.push_back(n);
+	std::deque<MyNode> frontierDjikstra;
+	frontierDjikstra.push_back(n);
 
 
 	int it = 0;
 	MyNode itNode = n;
 
-	while (!visitedCells.empty()) {
+	while (!frontierDjikstra.empty()) {
 
-		std::deque<MyNode> frontierPriority;
+		MyNode cheapestNode = frontierDjikstra[0];
+		int cheapI = 0;
 
+		for (int i = 0; i < frontierDjikstra.size() - 1; i++) {
+			if (frontierDjikstra[i].cost < cheapestNode.cost) {
+				cheapestNode = frontierDjikstra[i];
+				cheapI = i;
+			}
+		}
+		itNode = frontierDjikstra[cheapI];
+		frontierDjikstra.erase(frontierDjikstra.begin() + cheapI);
+		visitedCells.push_back(itNode);
+
+
+		bool alreadyVisited = false;
+		bool alreadyFrontier = false;
 		Vector2D point2 = (0, 0);
 		point2.x = itNode.position.x;
 		point2.y = itNode.position.y - 1.0f;
 		if (isValidCell(point2)) {
-			MyNode n2 = MyNode(point2, &itNode, getCostCell(point2), 0);
+			MyNode n2 = MyNode(point2, &itNode, getCostCell(point2), itNode.costSoFar + getCostCell(point2));
 			if (point2 == end) {
-				frontierPriority.push_back(n2);
+				visitedCells.push_back(n2);
 				break;
 			}
-			frontierPriority.push_back(n2);
+			for (int i = 0; i < visitedCells.size(); i++) {
+				if (visitedCells[i].position == point2) {
+					alreadyVisited = true;
+					break;
+				}
+			}
+			if (!alreadyVisited) {
+				for (int i = 0; i < frontierDjikstra.size(); i++) {
+					if (frontierDjikstra[i].position == point2 ) {
+						if (n2.cost + n2.parent->costSoFar < frontierDjikstra[i].costSoFar){
+							frontierDjikstra.erase(frontierDjikstra.begin() + i);
+							//n2.costSoFar = n2.cost + n2.parent->costSoFar;
+							frontierDjikstra.push_back(n2);
+						}
+						alreadyFrontier = true;
+						break;
+					}
+				}
+				if (!alreadyFrontier) {
+					//n2.costSoFar = n2.cost + n2.parent->costSoFar;
+					frontierDjikstra.push_back(n2);
+				}
+			}
 		}
-		
-	
+
+		alreadyVisited = false;
+		alreadyFrontier = false;
 		Vector2D point4 = (0, 0);
 		point4.x = itNode.position.x - 1.0f;
 		point4.y = itNode.position.y;
 		if (isValidCell(point4)) {
-			MyNode n4 = MyNode(point4, &itNode, getCostCell(point4), 0);
+			MyNode n4 = MyNode(point4, &itNode, getCostCell(point4), itNode.costSoFar + getCostCell(point4));
 			if (point4 == end) {
-				frontierPriority.push_back(n4);
+				visitedCells.push_back(n4);
 				break;
 			}
-			frontierPriority.push_back(n4);
+			for (int i = 0; i < visitedCells.size() - 1; i++) {
+				if (visitedCells[i].position == point4) {
+					alreadyVisited = true;
+					break;
+				}
+			}
+			if (!alreadyVisited) {
+				for (int i = 0; i < frontierDjikstra.size(); i++) {
+					if (frontierDjikstra[i].position == point4) {
+						if (n4.cost + n4.parent->costSoFar < frontierDjikstra[i].costSoFar) {
+							frontierDjikstra.erase(frontierDjikstra.begin() + i);
+							//n4.costSoFar = n4.cost + n4.parent->costSoFar;
+							frontierDjikstra.push_back(n4);
+						}
+						alreadyFrontier = true;
+						break;
+					}
+				}
+				if (!alreadyFrontier) {
+					//n4.costSoFar = n4.cost + n4.parent->costSoFar;
+					frontierDjikstra.push_back(n4);
+				}
+			}
 		}
-		
-		
+
+		alreadyVisited = false;
+		alreadyFrontier = false;
 		Vector2D point6 = (0, 0);
 		point6.x = itNode.position.x + 1.0f;
 		point6.y = itNode.position.y;
 		if (isValidCell(point6)) {
-			MyNode n6 = MyNode(point6, &itNode, getCostCell(point6), 0);
+			MyNode n6 = MyNode(point6, &itNode, getCostCell(point6), itNode.costSoFar+ getCostCell(point6));
 			if (point6 == end) {
-				frontierPriority.push_back(n6);
+				visitedCells.push_back(n6);
 				break;
 			}
-			frontierPriority.push_back(n6);
+			for (int i = 0; i < visitedCells.size(); i++) {
+				if (visitedCells[i].position == point6) {
+					alreadyVisited = true;
+				}
+			}
+			if (!alreadyVisited) {
+				for (int i = 0; i < frontierDjikstra.size(); i++) {
+					if (frontierDjikstra[i].position == point2) {
+						if (n6.cost + n6.parent->costSoFar < frontierDjikstra[i].costSoFar) {
+							frontierDjikstra.erase(frontierDjikstra.begin() + i);
+							//n6.costSoFar = n6.cost + n6.parent->costSoFar;
+							frontierDjikstra.push_back(n6);
+						}
+						alreadyFrontier = true;
+						break;
+					}
+				}
+				if (!alreadyFrontier) {
+					//n6.costSoFar = n6.cost + n6.parent->costSoFar;
+					frontierDjikstra.push_back(n6);
+				}
+			}
 		}
-		
-				
+
+		alreadyVisited = false;
+		alreadyFrontier = false;
 		Vector2D point8 = (0, 0);
 		point8.x = itNode.position.x;
 		point8.y = itNode.position.y + 1.0f;
 		if (isValidCell(point8)) {
-			MyNode n8 = MyNode(point8, &itNode, getCostCell(point8), 0);
+			MyNode n8 = MyNode(point8, &itNode, getCostCell(point8), itNode.costSoFar + getCostCell(point8));
 			if (point8 == end) {
-				frontierPriority.push_back(n8);
+				visitedCells.push_back(n8);
 				break;
 			}
-			frontierPriority.push_back(n8);
-		}
-		
-		MyNode cheapestNode = frontierPriority[0];
-		int cheapI = 0;
-
-		for (int i = 0; i < frontierPriority.size() - 1; i++) {
-			if (frontierPriority[i].cost < cheapestNode.cost) {
-				cheapestNode = frontierPriority[i];
-				cheapI = i;
-			}
-		}
-		frontierPriority.erase(frontierPriority.begin() + cheapI);
-
-		bool alreadyVisited = false;
-		for (int i = 0; i < visitedCells.size(); i++) {
-			if (visitedCells[i].position == cheapestNode.position) {
-				alreadyVisited = true;
-				if (visitedCells[i].costSoFar >= cheapestNode.parent->costSoFar + cheapestNode.cost) {
-					visitedCells.push_back(cheapestNode);
-					visitedCells[visitedCells.size() - 1].costSoFar = itNode.costSoFar + visitedCells[visitedCells.size() - 1].cost;
-					itNode = visitedCells[visitedCells.size() - 1];
+			for (int i = 0; i < visitedCells.size(); i++) {
+				if (visitedCells[i].position == point8) {
+					alreadyVisited = true;
+					break;
 				}
-				else {
-					itNode = visitedCells[i];
+			}
+			if (!alreadyVisited) {
+				for (int i = 0; i < frontierDjikstra.size(); i++) {
+					if (frontierDjikstra[i].position == point8) {
+						if (n8.cost + n8.parent->costSoFar < frontierDjikstra[i].costSoFar) {
+							frontierDjikstra.erase(frontierDjikstra.begin() + i);
+							//n8.costSoFar = n8.cost + n8.parent->costSoFar;
+							frontierDjikstra.push_back(n8);
+						}
+						alreadyFrontier = true;
+						break;
+					}
+				}
+				if (!alreadyFrontier) {
+					//n8.costSoFar = n8.cost + n8.parent->costSoFar;
+					frontierDjikstra.push_back(n8);
 				}
 			}
 		}
-		if (!alreadyVisited) {
-			visitedCells.push_back(cheapestNode);
-			visitedCells[visitedCells.size() - 1].costSoFar = itNode.costSoFar + visitedCells[visitedCells.size() - 1].cost;
-			itNode = visitedCells[visitedCells.size() - 1];
-		}
-		alreadyVisited = false;
-		
 	}
+
 
 	std::vector<Vector2D> points;
 	bool isStart = false;
 	bool pointFound = false;
 
-
 	itNode = visitedCells[visitedCells.size() - 1];
 
-	while (!isStart) {
+	/*while (!isStart) {
 		Vector2D tempPoint = itNode.position;
 		points.push_back(tempPoint);
-		Vector2D comesFrom = itNode.parent->position;
-		if (comesFrom == start) {
-			isStart = true;
+		if (itNode.parent != nullptr) {
+			Vector2D comesFrom = itNode.parent->position;
+			if (comesFrom == start) {
+				isStart = true;
+				break;
+			}
+			itNode = *itNode.parent;
 		}
-		itNode = *itNode.parent;
+		else {
+			points.push_back(tempPoint);
+			break;
+		}
 
+	}*/
+	while (true) {
+		points.push_back(itNode.position);
+		if (itNode.parent != nullptr) {
+			itNode = *itNode.parent;
+		}
+		else {
+			break;
+		}
 	}
 
 	Path myPath;
-	for (int i = points.size() - 1; i >= 0; i--) {
+	for (int i = points.size(); i >= 0; i--) {
 		myPath.points.push_back(points[i]);
 	}
 
